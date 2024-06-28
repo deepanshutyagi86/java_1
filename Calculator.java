314a2d7adfdcf00e01093ed313d8067041fcb4c5
deepanshutyagi86 <deepanshutyagi0784@gmail.com>
2024-06-28 13:57:33 +0530

    my second prog

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+  /**
+   * A description of the entire Java function.
+   *
+   * @param  args  description of parameter
+   * @return         description of return value
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
